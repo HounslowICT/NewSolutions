ac154b5fff4ccd5b89fbd2519e60b57b11893088
JorgeS <beakman23@hotmail.com>
2025-01-19 17:25:00 +0000

    First Full Version Commited

--- solutions/admin.py
+++ solutions/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Solution
 
+class SolutionsAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'solution', 'clase', 'date', 'link') #Add the field you want to display in the Admin area.
 
-admin.site.register(Solution)
+admin.site.register(Solution, SolutionsAdmin)
