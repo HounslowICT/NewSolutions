d2a709301014122190d90ebb68e22167248a445e
JorgeS <beakman23@hotmail.com>
2025-01-22 22:44:29 +0000

    Add dj-database-url

--- lbhsolutions/settings.py
+++ lbhsolutions/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 # import django_heroku
 # import  dj_database_path
 
@@ -69,12 +70,20 @@ WSGI_APPLICATION = 'lbhsolutions.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
