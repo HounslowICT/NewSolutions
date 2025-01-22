1a519aa367474595515caff8c9ec74af4d60efd6
JorgeS <beakman23@hotmail.com>
2025-01-22 23:34:47 +0000

    Add Final Migration

--- lbhsolutions/settings.py
+++ lbhsolutions/settings.py
@@ -71,12 +71,6 @@ DATABASES = {
 }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
